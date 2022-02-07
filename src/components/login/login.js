c87a51d40f87f10eb9a91f44684d031792e5f28d
hatim <hatim.joubair2@gmail.com>
2022-02-06 19:51:37 -0600

    fixadd search features

---
+++ src/components/login/login.js
@@ -0,0 +1,37 @@
+import react from 'react';
+import React, { Component } from 'react';
+
+export default class login extends Component {
+  render() {
+    return (
+    <react.Fragment>
+     <div className="row">
+        <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+          <div className="card border-0 shadow rounded-3 my-5">
+            <div className="card-body p-4 p-sm-5">
+              <h5 className="card-title text-center mb-5 fw-light fs-5">Welcome</h5>
+              <form>
+                <div className="form-floating mb-3">
+                  <input  name="email" className="form-control" placeholder="Email Address"/>
+                  <label for="floatingInput">Email</label>
+                </div>
+                <div className="form-floating mb-3">
+                  <input name="password" type="password" class="form-control" placeholder="password"/>
+                  <label for="floatingPassword">Password</label>
+                </div>
+                <div className="d-grid">
+                  <button className="btn btn-primary btn-login text-uppercase fw-bold" type="submit" >Sign in</button>
+                </div>
+              </form>
+            </div><br/>
+
+            <div className="d-grid">
+              <a href="/create" class="btn btn-primary btn-login text-uppercase fw-bold">Create Account</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </react.Fragment>
+    )
+  }
+}
